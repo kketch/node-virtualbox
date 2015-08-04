ec95930a2e00938717d927f547e8954e4ab657f3
kketch <wennerkevin@gmail.com>
2015-08-04 15:34:08 +0200

    updated test for "exec"

--- test/exec.js
+++ test/exec.js
@@ -5,21 +5,24 @@ var virtualbox = require('../lib/virtualbox'),
     vm         = args[0],
     user       = args[1],
     pass       = args[2],
-    ostype     = virtualbox.guestproperty.os(vm),
     path;
 
-if (ostype === "windows") {
-  path = "C:\\Program Files\\Internet Explorer\\iexplore.exe";
-} else if (ostype === "mac") {
-  path = "Safari.app";
-} else {
-  path = "whoami";
-}
+virtualbox.guestproperty.os(vm, function(ostype) {
 
-virtualbox.start(vm, function(){
-  virtualbox.exec({ 'vm': vm, 'user': user, 'passwd': pass, 'path': path, 'params': [args[1] || 'http://google.com'] }, function(error){
-    if(error) {
-      throw error;
-    }
+  if (ostype === "windows") {
+    path = "C:\\Program Files\\Internet Explorer\\iexplore.exe";
+  } else if (ostype === "mac") {
+    path = "Safari.app";
+  } else {
+    path = "whoami";
+  }
+
+  virtualbox.start(vm, function(){
+    virtualbox.exec({ 'vm': vm, 'user': user, 'passwd': pass, 'path': path, 'params': [args[1] || 'http://google.com'] }, function(error){
+      if(error) {
+        throw error;
+      }
+    });
   });
+
 });
